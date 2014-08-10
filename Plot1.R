067714d43801b68b2e0fab4faf08ebba0a7e200e
rahulmbe <rahulgupta@mbe-du.org>
2014-08-10 21:47:13 +0530

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,51 @@
+# Data
+
+## Folder
+if (!file.exists("./data")) {
+    dir.create("./data")
+}
+
+## Download
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(url, destfile="electric_power_consumption.zip")
+
+## Unzip file
+unzip("electric_power_consumption.zip", exdir="./data")
+
+## Load file
+pow <- read.table("./data/household_power_consumption.txt", sep=";", header=T)
+
+## Reformat date
+pow$Date <- as.Date(pow$Date, format="%d/%m/%Y")
+
+## Subset data from 2007-02-01 and 2007-02-02
+limited <- subset(pow, Date=="2007-02-01" | Date=="2007-02-02")
+
+## Reformat time
+
+### Merge date and time together
+limited$DateTime <- paste(limited$Date, limited$Time, sep=":")
+
+### Strip time
+limited$DateTime <- strptime(limited$DateTime, format="%Y-%m-%d:%H:%M:%S")
+
+limited$Date <- NULL
+limited$Time <- NULL
+
+## Convert "?" to NA
+limited[limited=="?"] <- NA
+
+## Convert classes to numeric
+for (i in 1:7) {
+    limited[, i] <- as.character(limited[, i])
+    limited[, i] <- as.numeric(limited[, i])
+}
+
+str(limited)
+
+# Plot 1
+hist(limited$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+# Copy to a PNG file
+dev.copy(png, file="plot1.png")
+dev.off()
